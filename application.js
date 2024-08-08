8fd5a7c7cca8cefcc9cbfe899deae25f13e851ab
ChiaAn <chiaan123@gmail.com>
2024-08-09 00:27:45 +0800

    Add nav bar and bgv to contact, apply

--- application.js
+++ application.js
@@ -37,3 +37,23 @@ document.addEventListener('DOMContentLoaded', () => {
         form.reset();
     });
 });
+
+/* js for slow down bgv */
+document.addEventListener('DOMContentLoaded',function(){
+    const video = document.getElementById('bgv')
+    video.playbackRate = 0.3; //slow down the video half speed
+})
+/* js for slow down bgv */
+
+
+/* js for nav bar */
+var header = document.getElementById("navigation");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+/* js for nav bar */
